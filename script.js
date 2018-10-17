63c12c243682c4e8913cce6b48ea017235e24e8a
David Sng <davidsnghs@gmail.com>
2018-10-17 16:16:48 +0800

    x

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+import {without} from 'lodash '
+
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
+
 
 function setGradient() {
 	body.style.background =
