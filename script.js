ff246263503b5b9d19ab69b539b0086d9a9133cc
David Sng <davidsnghs@gmail.com>
2018-10-17 16:15:12 +0800

    x

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
 
+import {without} from 'lodash'
+
+
 function setGradient() {
 	body.style.background =
 		"linear-gradient(to right, "
